240f72fa5d839cfbd70d615002ed658ff2364cb1
Alexander <jinmaster707@gmail.com>
2021-04-16 15:16:58 +0600

    Added render static files and create .gitgnore for heroku deploy

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import trainDataRouter from './train_data/trainDataRouter.js';
 
-const PORT = 8000;
 const app = express();
+const PORT = process.env.PORT || 8000;
+app.use(express.static('public'));
 app.use('/trainData', trainDataRouter);
 app.use(express.json());
 
 async function startApp() {
   try {
-    app.listen(PORT, '127.0.0.1', () => console.log('server start'));
+    app.listen(PORT, () => console.log('server start'));
   } catch (err) {
     console.log(err);
   }
